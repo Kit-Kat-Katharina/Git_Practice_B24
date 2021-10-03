fdfd0f3dfd320142bb68d9d8967d3921111d2ddd
HP Laptop <katharina.slava@gmail.com>
2021-10-03 16:20:04 -0400

    update: made car in Master branch

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,13 +2,13 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // this is car class from master branch
 
     //we will have conflict
 
     //because there is different text in different branches for car
-=======
+
     //this is our car from james branch
 
     //more text
@@ -16,6 +16,5 @@ public class Car {
     //more text
 
 
->>>>>>> james
 
 }

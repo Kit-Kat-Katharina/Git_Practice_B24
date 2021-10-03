3e84ae5ccc7c14716d78ef31ee235bf0ee70fa04
HP Laptop <katharina.slava@gmail.com>
2021-10-03 12:23:54 -0400

    updating second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -5,15 +5,16 @@ public class Second {
 
 
     public void m() {
+        System.out.println("first method");
 
 
     }
 
     public void m(int a){
-
+        System.out.println("second method");
     }
     public void m (String s) {
-
+        System.out.println("third method");
 
 
     }
